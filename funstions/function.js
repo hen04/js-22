7289e2bbddf7cdc4c2128f4490ab752cd5312f6d
DESKTOP-NDUN9JV\Nadya <nadia_degtiareva@rambler.ru>
2022-08-30 19:50:04 +0300

    function

---
+++ funstions/function.js
@@ -0,0 +1,10 @@
+function calculateTotal() {
+	const total = 20 + 20;
+	return total; // возврат значения позволяет использовать переменную вне функции
+}
+
+const total = calculateTotal();
+
+console.log(total);
+
+
